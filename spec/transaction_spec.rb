db3aa9fd934bf61c073f25de4c4128066421e84a
Jihin Ip <jim_ip20@hotmail.com>
2018-05-21 16:44:54 +0100

    Passing tests for deposit method, with sum and date arguments

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,16 +7,16 @@ describe ".Transaction" do
   describe "#deposit" do
 
     it "can deposit 10" do
-      account = Transaction.new
-      account.deposit(10.00)
-      expect(account.balance).to eq(10.00)
+      transaction = Transaction.new
+      transaction.deposit(10.00, "miaow")
+      expect(transaction.balance).to eq(10.00)
     end
 
-    # it "deposit method has a date" do
-    #   account = Transaction.new
-    #   account.deposit(10.00, 30-06-2013)
-    #
-    # end
+    it "deposit method has a date" do
+      transaction = Transaction.new
+      transaction.deposit(10.00, "30-06-2013")
+      expect(transaction.date).to eq("30-06-2013")
+    end
 
   end
 
