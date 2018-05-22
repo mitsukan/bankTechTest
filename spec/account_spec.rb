d04c0a6185dfd90fd7795923e9cbd0c98627976e
Jihin Ip <jim_ip20@hotmail.com>
2018-05-22 11:52:53 +0100

    Adding test for .newEntry to diferentiate between credit and debit

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -18,12 +18,23 @@ describe `#Account` do
 
     it "takes 2 transactions and calculates balance correctly" do
       account = Account.new
-      amount = 10.00
+      amount = 120.00
       date = "23-04-2020"
       account.newEntry(date,amount)
       account.newEntry(date,amount)
-      expect(account.transactions).to eq([["23-04-2020", 10.0, 10.0], ["23-04-2020", 10.0, 20.0]])
+      expect(account.transactions).to eq([["23-04-2020", 120.0, 120.0], ["23-04-2020", 120.0, 240.0]])
     end
+
+    it "differentiates between credit and debit amount" do
+      account = Account.new
+      positive_amount = 110.00
+      negative_amount = -50.00
+      date = "04-04-0404"
+      account.newEntry(date, positive_amount)
+      account.newEntry(date, negative_amount)
+      expect(account.transactions). to eq([["04-04-0404", 110.0, 0, 110.0],["04-04-0404", 0, 50.0, 60.0]])
+    end
+
   end
 
 end
