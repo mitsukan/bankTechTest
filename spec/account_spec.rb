1b5910bcef52d952891a20de552b69b33226b67b
Jihin Ip <jim_ip20@hotmail.com>
2018-05-22 11:39:58 +0100

    Adding testing for multiple transactions to test balance function

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,10 +10,19 @@ describe `#Account` do
   describe ".newEntry" do
     it "takes information from transaction and store in account" do
       account = Account.new
-      amount = 10
+      amount = 10.00
       date = "12-04-2020"
       account.newEntry(date, amount)
-      expect(account.transactions).to eq([["12-04-2020", 10, 10]])
+      expect(account.transactions).to eq([["12-04-2020", 10.0, 10.0]])
+    end
+
+    it "takes 2 transactions and calculates balance correctly" do
+      account = Account.new
+      amount = 10.00
+      date = "23-04-2020"
+      account.newEntry(date,amount)
+      account.newEntry(date,amount)
+      expect(account.transactions).to eq([["23-04-2020", 10.0, 10.0], ["23-04-2020", 10.0, 20.0]])
     end
   end
 
