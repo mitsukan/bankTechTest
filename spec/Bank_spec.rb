fbd937ad30090a6f6fe8a6f10213a6d02a426208
Jihin Ip <jim_ip20@hotmail.com>
2018-05-23 12:02:10 +0100

    Fixing bank_spec, adding feature tests for bank

--- spec/Bank_spec.rb
+++ spec/Bank_spec.rb
@@ -9,17 +9,40 @@ describe "#Bank" do
     bank.account.newEntry("14-01-2012", -500.0)
     expect(bank.account.transactions).to eq([["10-01-2012", "1000.00", "0.00", "1000.00"], ["13-01-2012", "2000.00", "0.00", "3000.00"], ["14-01-2012", "0.00", "500.00", "2500.00"]])
   end
-  #
-  # it "prints the 3 transactions" do
-  #   bank = Bank.new
-  #   bank.account.newEntry("10-01-2012", 1000.0)
-  #   bank.account.newEntry("13-01-2012", 2000.0)
-  #   bank.account.newEntry("14-01-2012", -500.0)
-  #   expect do
-  #     bank.printer.entry(bank.account.transactions)
-  #   end.to output("10-01-2012 || 1000.00 || || 1000.00\n" "13-01-2012 || 2000.00 || || 3000.00\n" "14-01-2012 || || 500.00 || 2500.00\n").to_stdout
-  # end
 
+  it "prints the 3 transactions" do
+    bank = Bank.new
+    bank.account.newEntry("10-01-2012", 1000.0)
+    bank.account.newEntry("13-01-2012", 2000.0)
+    bank.account.newEntry("14-01-2012", -500.0)
+    expect do
+      bank.printer.entry(bank.account.transactions)
+    end.to output("14-01-2012 || || 500.00 || 2500.00\n" "13-01-2012 || 2000.00 || || 3000.00\n"  "10-01-2012 || 1000.00 || || 1000.00\n").to_stdout
+  end
+
+  describe ".enter_transaction" do
+
+    it "Adds a transaction into the account" do
+      bank = Bank.new
+      bank.enter_transaction("10-01-2012", 1000.0)
+      expect(bank.account.transactions).to eq([["10-01-2012", "1000.00", "0.00", "1000.00"]])
+    end
+
+  end
+
+  describe ".print_statement" do
+
+    it "Prints the statement" do
+      bank = Bank.new
+      bank.enter_transaction("10-01-2012", 1000.0)
+      bank.enter_transaction("13-01-2012", 2000.0)
+      bank.enter_transaction("14-01-2012", -500.0)
+      expect do
+        bank.print_statement
+      end.to output("14-01-2012 || || 500.00 || 2500.00\n" "13-01-2012 || 2000.00 || || 3000.00\n"  "10-01-2012 || 1000.00 || || 1000.00\n").to_stdout
+    end
+
+  end
 
 end
 
